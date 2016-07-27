dd764e66b13e0fd73a34ca6b5276c4fb1ab563ef
Tomasz Janisiewicz <Tomasz.Janisiewicz.ctr@sabre.com>
2016-07-27 13:58:18 +0200

    init src

---
+++ src/main/java/pl/webframe/Application.java
@@ -0,0 +1,15 @@
+package pl.webframe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Created by SG0952598 on 2016-07-27.
+ */
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
---
+++ src/main/java/pl/webframe/Greeting.java
@@ -0,0 +1,23 @@
+package pl.webframe;
+
+/**
+ * Created by SG0952598 on 2016-07-27.
+ */
+public class Greeting {
+
+    private final long id;
+    private final String content;
+
+    public Greeting(long id, String content) {
+        this.id = id;
+        this.content = content;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public String getContent() {
+        return content;
+    }
+}
